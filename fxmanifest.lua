52a128053b1daf33a49982ba04a1674c06d8af76
TehGuardian <72229591+TehGuardian@users.noreply.github.com>
2025-04-18 18:48:12 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version("cerulean")
 game("gta5")
 lua54("yes")
 client_script("@mythic-base/components/cl_error.lua")
-client_script("@cm-pwnzor/client/check.lua")
+client_script("@mythic-pwnzor/client/check.lua")
 server_script("@oxmysql/lib/MySQL.lua")
 
 client_scripts({
