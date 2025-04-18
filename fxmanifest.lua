eaea26fa11975d875caf169738da4ea37541b8a8
TehGuardian <72229591+TehGuardian@users.noreply.github.com>
2025-04-18 18:34:55 -0400

    Conversion

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version("cerulean")
+game("gta5")
+lua54("yes")
+client_script("@mythic-base/components/cl_error.lua")
+client_script("@cm-pwnzor/client/check.lua")
+server_script("@oxmysql/lib/MySQL.lua")
+
+client_scripts({
+	"shared/**/*.lua",
+	"client/**/*.lua",
+})
+
+server_scripts({
+	"shared/**/*.lua",
+	"server/**/*.lua",
+})
+
+ui_page("ui/dist/index.html")
+
+files({ "ui/dist/index.html", "ui/dist/*.js", "ui/dist/*.png", "ui/dist/*.webp" })
